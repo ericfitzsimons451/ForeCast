c07b0f8c5c2405fa370c2f92690b13cda59479d9
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-04-13 12:22:06 -0600

    adds MSTP test in App

--- src/Containters/App/App.test.js
+++ src/Containters/App/App.test.js
@@ -1,1 +1,22 @@
+import React from 'react'
+import { App, mapStateToProps } from './App'
 
+describe('App', () => {
+  describe('mapStateToProps', () => {
+    it('should mapStateToProps', () => {
+      const mockState = {}
+      const updatedState = {
+        isLoading: false,
+        hasError: false,
+        currentWeather: {
+          city: 'Denver',
+          sunrise: '11:30'
+        }
+      }
+      const firstState = mapStateToProps(mockState)
+      expect(firstState).toEqual({})
+      const newState = mapStateToProps(updatedState)
+      expect(newState).toEqual(updatedState)
+    })
+  })
+})
